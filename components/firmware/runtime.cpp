8b9b284f4d99df9fd99614ec2f4bbda1387ed4ca
kubgus <business@gustafik.com>
2025-03-11 14:18:30 +0100

    Add welcome screen

--- components/firmware/runtime.cpp
+++ components/firmware/runtime.cpp
@@ -12,13 +12,22 @@ Runtime::Runtime() :
     m_Display(FW_SCREEN_WIDTH, FW_SCREEN_HEIGHT, &Wire, FW_PIN_RESET)
 {
     ESP_LOGI(TAG, "Initializing...");
+    Wire.begin(FW_PIN_SDA, FW_PIN_SCL, FW_I2C_FREQUENCY);
 
+    ESP_LOGD(TAG, "Initializing display...");
     ASSERT(
         m_Display.begin(SSD1306_SWITCHCAPVCC, FW_SCREEN_ADDRESS),
-        "SSD1306 allocation failed!"
+        "SSD1306 allocation failed! (display)"
     );
     m_Display.setRotation(2); // Upside down
-    m_Display.clearDisplay();
+    m_Display.clearDisplay(); // Clean screen
+    // Welcome message
+    m_Display.setCursor(10, 10);
+    m_Display.setTextColor(WHITE);
+    m_Display.setTextSize(2);
+    m_Display.print("Welcome!");
+    // Initial draw
+    m_Display.display();
 }
 
 Runtime::~Runtime() {
