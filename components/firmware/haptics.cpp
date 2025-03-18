db58fdb57aac4d1abc5935fbfe97da4542568167
kubgus <business@gustafik.com>
2025-03-18 22:28:23 +0100

    Change haptics to work on a 0.0 - 1.0 basis (based on resolution) (it's really nice, actually)

--- components/firmware/haptics.cpp
+++ components/firmware/haptics.cpp
@@ -4,11 +4,15 @@
 
 #include "esp32-hal-ledc.h"
 
-Haptics::Haptics(uint32_t frequency, uint8_t resolution) {
-    ledcAttachChannel(FW_PIN_VMOTOR1, 5000, 8, FW_CHANNEL_VMOTOR1);
-    ledcAttachChannel(FW_PIN_VMOTOR2, 5000, 8, FW_CHANNEL_VMOTOR2);
-    ledcAttachChannel(FW_PIN_VMOTOR3, 5000, 8, FW_CHANNEL_VMOTOR3);
-    ledcAttachChannel(FW_PIN_VMOTOR4, 5000, 8, FW_CHANNEL_VMOTOR4);
+Haptics::Haptics(uint32_t frequency, uint8_t resolution) :
+    m_Frequency(frequency),
+    m_Resolution(resolution),
+    m_MaxIntensity((1U << resolution) - 1) // (2^resolution) - 1
+{
+    ledcAttachChannel(FW_PIN_VMOTOR1, m_Frequency, m_Resolution, FW_CHANNEL_VMOTOR1);
+    ledcAttachChannel(FW_PIN_VMOTOR2, m_Frequency, m_Resolution, FW_CHANNEL_VMOTOR2);
+    ledcAttachChannel(FW_PIN_VMOTOR3, m_Frequency, m_Resolution, FW_CHANNEL_VMOTOR3);
+    ledcAttachChannel(FW_PIN_VMOTOR4, m_Frequency, m_Resolution, FW_CHANNEL_VMOTOR4);
 
     ledcWrite(FW_PIN_VMOTOR1, 0);
     ledcWrite(FW_PIN_VMOTOR2, 0);
@@ -16,23 +20,31 @@ Haptics::Haptics(uint32_t frequency, uint8_t resolution) {
     ledcWrite(FW_PIN_VMOTOR4, 0);
 }
 
-void Haptics::SetMotor1(uint8_t intensity) {
-    ledcWrite(FW_PIN_VMOTOR1, intensity);
+void Haptics::SetMotorByPin(uint8_t pin, float intensity) {
+    if (intensity < 0.0f) intensity *= -1.0f;
+    if (intensity > 1.0f) intensity = 1.0f;
+    uint32_t rawIntensity = (uint32_t)(intensity * m_MaxIntensity);
+
+    ledcWrite(pin, rawIntensity);
 }
 
-void Haptics::SetMotor2(uint8_t intensity) {
-    ledcWrite(FW_PIN_VMOTOR2, intensity);
+void Haptics::SetMotor1(float intensity) {
+    SetMotorByPin(FW_PIN_VMOTOR1, intensity);
 }
 
-void Haptics::SetMotor3(uint8_t intensity) {
-    ledcWrite(FW_PIN_VMOTOR3, intensity);
+void Haptics::SetMotor2(float intensity) {
+    SetMotorByPin(FW_PIN_VMOTOR2, intensity);
 }
 
-void Haptics::SetMotor4(uint8_t intensity) {
-    ledcWrite(FW_PIN_VMOTOR4, intensity);
+void Haptics::SetMotor3(float intensity) {
+    SetMotorByPin(FW_PIN_VMOTOR3, intensity);
 }
 
-void Haptics::SetAll(uint8_t intensity) {
+void Haptics::SetMotor4(float intensity) {
+    SetMotorByPin(FW_PIN_VMOTOR4, intensity);
+}
+
+void Haptics::SetAll(float intensity) {
     SetMotor1(intensity);
     SetMotor2(intensity);
     SetMotor3(intensity);
--- components/firmware/haptics.h
+++ components/firmware/haptics.h
@@ -6,9 +6,55 @@ class Haptics {
 public:
     Haptics(uint32_t frequency = 5000, uint8_t resolution = 8);
 
-    void SetMotor1(uint8_t intensity);
-    void SetMotor2(uint8_t intensity);
-    void SetMotor3(uint8_t intensity);
-    void SetMotor4(uint8_t intensity);
-    void SetAll(uint8_t intensity);
+    /**
+     * @brief Set vibration intensity of motor 1.
+     *
+     * Intensity is automatically absoluted and capped at 1.0.
+     *
+     * @param intensity Intensity of the motor (0.0 - 1.0)
+     */
+    void SetMotor1(float intensity);
+
+    /**
+     * @brief Set vibration intensity of motor 2.
+     *
+     * Intensity is automatically absoluted and capped at 1.0.
+     *
+     * @param intensity Intensity of the motor (0.0 - 1.0)
+     */
+    void SetMotor2(float intensity);
+
+    /**
+     * @brief Set vibration intensity of motor 3.
+     *
+     * Intensity is automatically absoluted and capped at 1.0.
+     *
+     * @param intensity Intensity of the motor (0.0 - 1.0)
+     */
+    void SetMotor3(float intensity);
+
+    /**
+     * @brief Set vibration intensity of motor 4.
+     *
+     * Intensity is automatically absoluted and capped at 1.0.
+     *
+     * @param intensity Intensity of the motor (0.0 - 1.0)
+     */
+    void SetMotor4(float intensity);
+
+    /**
+     * @brief Set vibration intensity of all motors.
+     *
+     * Intensity is automatically absoluted and capped at 1.0.
+     *
+     * @param intensity Intensity of the motors (0.0 - 1.0)
+     */
+    void SetAll(float intensity);
+private:
+    uint32_t m_Frequency;
+    uint8_t m_Resolution;
+
+    uint32_t m_MaxIntensity;
+
+    void SetMotorByPin(uint8_t pin, float intensity);
 };
